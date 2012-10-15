26ba6ca16f7871cd9bd49888c44c9c66c4f812ba
Matija Bozic <matijabozic@gmx.com>
2012-10-15 11:18:16 +0200

    Initial commit!

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	
+	// Include Autoload
+	
+	require_once('../system/Core/Autoload/Autoload.php');
+	
+	// Register class autoloading for PSR-0 compatible libraries	
+	
+	$psr0 = new Autoload();
+	$psr0->addLibrary('path/to/vendor/', 'Doctrine');
+	$psr0->addLibrary('path/to/vendor/', 'Zend');
+	$psr0->register();	
+	
+	// Register class autoloading for PEAR compatible libraries
+	
+	$pear = new Autoload();
+	$pear->setNamespaceSeparator('_');
+	$pear->addLibrary('path/to/vendor/', 'Twig');
+	$pear->register();
+	
+	// Thats it!
+	
+?>
